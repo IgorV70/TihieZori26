81b9fa0ca606319f550a65f2d3019e452b0dcc4b
VOTINTSEV <sinap@mail.ru>
2026-08-27 22:41:28 +0700

    Добавьте файлы проекта.

---
+++ TihieZori/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace TihieZori.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _options = options;
+        //protected readonly IConfiguration _configuration;
+        //public ApplicationDbContext(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (_configuration != null)
+            //{
+            //    object value = optionsBuilder.UseNpgsql(_configuration.GetConnectionString("db")
+            //    , x => x.MigrationsHistoryTable("__EgrulMigrationsHistory", "plain"));
+            //    //optionsBuilder.LogTo(s => Debug.Print(s));
+
+            //}
+            //else
+            //optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=tihiezori;Username=postgres;Password=postgres;");
+            if (this._options != null)
+            { 
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ TihieZori/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TihieZori.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
